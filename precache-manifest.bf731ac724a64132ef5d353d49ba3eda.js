13c80f0347bccc8cbcfc6d503b892132700fa808
William Huang <whuang7000@gmail.com>
2019-03-31 19:11:43 -0700

    Updates

---
+++ precache-manifest.bf731ac724a64132ef5d353d49ba3eda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "44f3de7a1893d6706567",
+    "url": "/oki-website/static/css/main.b52f5648.chunk.css"
+  },
+  {
+    "revision": "44f3de7a1893d6706567",
+    "url": "/oki-website/static/js/main.21df6cb2.chunk.js"
+  },
+  {
+    "revision": "57e144f172dc33ce0976",
+    "url": "/oki-website/static/js/runtime~main.7aaf8cb2.js"
+  },
+  {
+    "revision": "0a526b75501deb8c7f66",
+    "url": "/oki-website/static/js/2.b748a133.chunk.js"
+  },
+  {
+    "revision": "53056663094db8f7e3de0880c86baa1a",
+    "url": "/oki-website/static/media/oki.53056663.png"
+  },
+  {
+    "revision": "d1517c0a84c726a861c18121413c9c76",
+    "url": "/oki-website/static/media/TransparentWarmOki.d1517c0a.png"
+  },
+  {
+    "revision": "c3b8a1ecba658e98febd4e48c77def29",
+    "url": "/oki-website/index.html"
+  }
+];
